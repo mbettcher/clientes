37ccee4944dcd86f90fcc6f72ce9d800bfabae72
mbettcher <mtonon.pmg@gmail.com>
2024-02-14 09:53:13 -0300

    Criação do domínio Cliente e adiciona o Lombok ao projeto

---
+++ src/main/java/br/com/mtonon/clientes/model/entity/Cliente.java
@@ -0,0 +1,41 @@
+package br.com.mtonon.clientes.model.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 150)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column
+	private LocalDate dataCadastro;
+
+}
