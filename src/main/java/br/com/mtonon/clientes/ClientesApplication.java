7e2823fd04af39647e2e85f200c472008f7979f0
mbettcher <mtonon.pmg@gmail.com>
2024-02-14 10:33:41 -0300

    Testa conexão com a base de dados e inclui um cliente

--- src/main/java/br/com/mtonon/clientes/ClientesApplication.java
+++ src/main/java/br/com/mtonon/clientes/ClientesApplication.java
@@ -1,11 +1,32 @@
 package br.com.mtonon.clientes;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.mtonon.clientes.model.entity.Cliente;
+import br.com.mtonon.clientes.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
 
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repo) {
+		return args -> {
+			Cliente cliente = Cliente.builder()
+								.nome("Cliente Teste")
+								.cpf("11111111111")
+								.dataCadastro(LocalDate.now())
+								.build();
+			repo.save(cliente);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
 	}
